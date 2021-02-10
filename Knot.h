fc4f3b49fc2e641b5b4598dc2a61315db313a3aa
k3DW <braden.ganetsky@gmail.com>
2021-02-10 01:18:17 -0600

    Update Knot.h

--- Knot.h
+++ Knot.h
@@ -30,7 +30,7 @@ private:
 	void rotate180LeftToRight(GlyphVec2& glyphGrid, ijSignature) const;
 	void rotate90FromUpLeft(GlyphVec2& glyphGrid, ijSignature) const;
 
-	void tryGenerating(std::optional<GlyphVec2>& glyphGrid, ijSignature, const Side ignoreSides = Side::NONE, const GlyphFlag boolFlags = GlyphFlag::NONE) const;
+	void tryGenerating(std::optional<GlyphVec2>& glyphGrid, ijSignature, const Side ignoreSides = Side::NONE, const GlyphFlag boolFlags = GlyphFlag::NONE) const;	
 	void tryGeneratingDiag(std::optional<GlyphVec2>& glyphGrid, ijSignature, const bool fwdDiag, const Side ignoreSides = Side::NONE) const;
 	static inline bool inSelection(ijSignature, const int i, const int j);
 	static inline bool isEvenSegments(const int min, const int max);
