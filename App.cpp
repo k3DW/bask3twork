b3be87aacfd1e175250d690ddaf98adc4f791667
k3DW <braden.ganetsky@gmail.com>
2021-02-26 18:40:13 -0600

    Changed visual version to 0.5

--- App.cpp
+++ App.cpp
@@ -37,7 +37,7 @@ private:
 	MainWindow* main;
 
 	static constexpr int MAJOR_VERSION = 0;
-	static constexpr int MINOR_VERSION = 4;
+	static constexpr int MINOR_VERSION = 5;
 	static constexpr int PATCH_VERSION = 0;
 	static const wxString VERSION;
 
