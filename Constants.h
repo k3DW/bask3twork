c07fc5b178756e85dec2c88b3b37ae5da1e9270b
k3DW <braden.ganetsky@gmail.com>
2021-03-13 16:23:23 -0600

    Updated Constants.h with a few better comments

--- Constants.h
+++ Constants.h
@@ -24,8 +24,16 @@ using ull = unsigned long long;
 	XX(BackDiag, "Backward Diagonal") \
 	XX(FullSym, "Full Symmetry")
 
-/// All the types of symmetry which can be used in this program.
-/// The bit flags tell Knot::generate() how to generate.
+/** All the types of symmetry which can be used in this program.
+ * The bit flags tell Knot::generate() how to generate.
+ * In order from least-significant to most-significant bit, the bits mean the following.
+ * (1) This symmetry includes horizontal reflection.
+ * (2) This symmetry includes vertical reflection.
+ * (3) This symmetry includes 2-fold rotation.
+ * (4) This symmetry includes 4-fold rotation.
+ * (5) This symmetry includes forward diagonal reflection.
+ * (6) This symmetry includes backward diagonal reflection.
+ */
 enum class Symmetry : unsigned int {
 	NoSym		= 0b000000,	///< No symmetry
 	HoriSym		= 0b000001,	///< Mirror symmetry across the horizontal axis
@@ -53,8 +61,8 @@ constexpr inline bool operator&(Symmetry sym, unsigned int n)
 const int SCREEN_X = wxSystemSettings::GetMetric(wxSYS_SCREEN_X);	///< For future functionality, the screen size in the x direction
 const int SCREEN_Y = wxSystemSettings::GetMetric(wxSYS_SCREEN_Y);	///< For future functionality, the screen size in the y direction
 const wxSize MAX_SIZE{ SCREEN_X, SCREEN_Y };						///< For future functionality, the maximum size of the app window
-constexpr size_t MAX_H = 100;
-constexpr size_t MAX_W = 100;
+constexpr size_t MAX_H = 100;	///< The maximum height of the grid allowed in the program, in terms of tiles.
+constexpr size_t MAX_W = 100;	///< The maximum width of the grid allowed in the program, in terms of tiles
 
 // Constants for the colours of the program
 const wxColour BACKGROUND_COLOUR{ 240, 240, 240 };	///< The default colour of the background for the program
