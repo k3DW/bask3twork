0c19d10cbaa0c87c974b60f5518642384b00989b
k3DW <braden.ganetsky@gmail.com>
2022-07-28 13:11:41 -0500

    #24: Move Point and Selection into Selection.h

--- header/Glyphs.h
+++ header/Glyphs.h
@@ -1,22 +1,6 @@
 #pragma once
 #include "Glyph.h"
-
-struct Point
-{
-	int i;
-	int j;
-};
-
-struct Selection
-{
-	Point min;
-	Point max;
-
-	bool is_square() const { return max.i - min.i == max.j - min.j; }
-	bool is_full_selection(int rows, int columns) const { return min.i == 0 && min.j == 0 && max.i == rows - 1 && max.j == columns - 1; }
-
-	bool contains(Point point) const { return point.i >= min.i && point.i <= max.i && point.j >= min.j && point.j <= max.j; }
-};
+#include "Selection.h"
 
 class SymmetryChecker
 {
