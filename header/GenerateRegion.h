a1bcd1cfcb8af4d084cabf12b7c3bd1edff6148f
k3DW <braden.ganetsky@gmail.com>
2022-08-05 23:08:34 -0500

    #34: Remove SYMMETRIES macro by creating GenerateRegionButton class

--- header/GenerateRegion.h
+++ header/GenerateRegion.h
@@ -4,6 +4,8 @@
 
 class MainWindow;
 enum class Symmetry;
+class GenerateRegion;
+class GenerateRegionButton;
 
 /// The buttons inside the GenerateRegion allow the Knot \c generate functions to have no conditional operation, and just assume that the parameters are valid.
 /// Instead of checking for symmetry conditions within the generating function itself, the user is disallowed from pressing the button at all, unless the symmetry works.
@@ -12,11 +14,19 @@ class GenerateRegion : public wxStaticBoxSizer
 public:
 	GenerateRegion(MainWindow* parent);
 
-	void enable_buttons(Symmetry sym);
+	void enable_buttons(Symmetry symmetry);
 	void disable_buttons();
 
 private:
-	#define XX(Sym, desc) wxButton* Sym##Button;
-	SYMMETRIES
-	#undef XX
+	std::array<GenerateRegionButton*, 9> buttons;
+};
+
+class GenerateRegionButton : public wxButton
+{
+private:
+	friend class GenerateRegion;
+
+	GenerateRegionButton(MainWindow* parent, Symmetry symmetry, const char* label);
+
+	Symmetry symmetry;
 };
