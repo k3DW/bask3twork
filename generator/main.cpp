9fc1497f017fa34ae552da67182ee0cf1d52444e
k3DW <braden.ganetsky@gmail.com>
2022-08-07 01:59:57 -0500

    #36: Start some command line arg parsing

--- generator/main.cpp
+++ generator/main.cpp
@@ -1,10 +1,54 @@
 #include <iostream>
+#include <fstream>
+#include <filesystem>
+#include <string>
+#include <variant>
+#include <format>
 
-int main(int argc, char** argv)
+template <class T>
+using ErrorVariant = std::variant<T, std::string>;
+
+
+
+using InputPathsVariant = ErrorVariant<std::pair<std::filesystem::path, std::filesystem::path>>;
+
+InputPathsVariant get_input_paths(int argc, const char** argv)
 {
-	for (int i = 0; i < argc; i++)
-		std::cout << i << " -> " << argv[i] << "\n";
+	if (argc != 3)
+		return "Bask3twork generator must be given exactly 2 arguments:\n\t(1) The path of the CSV file; and\n\t(2) The path of the output directory";
 
-	if (argc == 0)
-		std::cout << "argc is 0\n";
+	std::filesystem::path csv_path = argv[1];
+	std::filesystem::path output_path = argv[2];
+
+	if (not std::filesystem::exists(csv_path))
+		return std::format(R"(The CSV file path does not exist: "{}")", csv_path.string());
+	if (not std::filesystem::is_regular_file(csv_path) || csv_path.extension() != ".csv")
+		return R"(The CSV file path must have ".csv" extension)";
+
+	if (not std::filesystem::exists(output_path))
+		return std::format(R"(The output path does not exist: "{}")", output_path.string());
+	if (not std::filesystem::is_directory(output_path))
+		return "The output path must be a directory";
+
+	return std::pair(std::move(csv_path), std::move(output_path));
+}
+
+
+
+int main(int argc, const char** argv)
+{
+	InputPathsVariant initial_paths = get_input_paths(argc, argv);
+	if (initial_paths.index() == 1)
+	{
+		std::cout << std::get<1>(initial_paths) << "\n";
+		return EXIT_FAILURE;
+	}
+	const auto& [csv_path, output_path] = std::get<0>(initial_paths);
+
+	std::ifstream csv = std::ifstream(csv_path);
+	if (not csv.is_open())
+	{
+		std::cout << std::format(R"(Could not open the CSV file: "{}")", csv_path.string());
+		return EXIT_FAILURE;
+	}
 }
