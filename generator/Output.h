9ad5d6acb44f2e210955884935b3ef13bc6d70ba
k3DW <braden.ganetsky@gmail.com>
2022-08-20 00:30:38 -0500

    #46: Create class OutputFormatter to encapsulate some of the formatting details

--- generator/Output.h
+++ generator/Output.h
@@ -6,41 +6,53 @@
 static std::string        get_string(int i)                  { return std::to_string(i); }
 static const std::string& get_string(const std::string& str) { return str; }
 
-template <class Collection, std::size_t Width = std::tuple_size_v<typename Collection::value_type>>
-static std::array<std::size_t, Width> get_max_sizes(const Collection& collection)
+template <class Collection>
+class OutputFormatter
 {
-	static constexpr auto get_element_sizes = []<std::size_t... Is>(std::index_sequence<Is...>)
-	{
-		return [](auto& element) -> std::array<std::size_t, Width>
-		{
-			return { get_string(get<Is>(element)).size()... };
-		};
-	}(std::make_index_sequence<Width>{});
+public:
+	using line_type = typename Collection::value_type;
+	static constexpr std::size_t width = std::tuple_size_v<line_type>;
+	using array_type = std::array<std::size_t, width>;
 
-	std::array<std::size_t, Width> max_sizes = {};
-	for (const auto& elem : collection)
+	OutputFormatter(const Collection& collection) : max_sizes(get_max_sizes(collection)) {}
+
+	template <std::size_t I>
+	std::string format(const line_type& line, std::string_view formatting) const
 	{
-		static constexpr auto max_fn = [](std::size_t a, std::size_t b) -> std::size_t { return std::max(a, b); };
-		std::ranges::transform(max_sizes, get_element_sizes(elem), max_sizes.begin(), max_fn);
+		std::string str = get_string(get<I>(line));
+		std::string formatted = std::vformat(formatting, std::make_format_args(str));
+		if (std::size_t padding = get<I>(max_sizes) - str.size(); padding != 0)
+			return std::format("{}{:{}}", formatted, "", padding);
+		else
+			return formatted;
 	}
-	return max_sizes;
-}
 
+private:
+	static array_type get_max_sizes(const Collection& collection)
+	{
+		static constexpr auto get_element_sizes = []<std::size_t... Is>(std::index_sequence<Is...>)
+		{
+			return[](auto& element)->array_type
+			{
+				return { get_string(get<Is>(element)).size()... };
+			};
+		}(std::make_index_sequence<width>{});
 
+		array_type max_sizes = {};
+		for (const auto& elem : collection)
+		{
+			static constexpr auto max_fn = [](std::size_t a, std::size_t b) -> std::size_t { return std::max(a, b); };
+			std::ranges::transform(max_sizes, get_element_sizes(elem), max_sizes.begin(), max_fn);
+		}
+		return max_sizes;
+	}
 
-const auto formatted = [](std::size_t max_size, std::string_view format, auto value) -> std::string
-{
-	std::string str = get_string(value);
-	std::string formatted = std::vformat(format, std::make_format_args(str));
-	if (std::size_t padding = max_size - str.size(); padding != 0)
-		return std::format("{}{:{}}", formatted, "", padding);
-	else
-		return formatted;
+	array_type max_sizes;
 };
 
 void output_all_glyphs(std::ofstream& all_glyphs_file, const ProcessedLines& processed_input_lines)
 {
-	const std::array<std::size_t, 11> max_sizes = get_max_sizes(processed_input_lines);
+	const OutputFormatter formatter(processed_input_lines);
 
 	all_glyphs_file << std::format("constexpr std::array<Glyph, {}> AllGlyphs =\n", processed_input_lines.size());
 	all_glyphs_file << "{\n";
@@ -49,21 +61,21 @@ void output_all_glyphs(std::ofstream& all_glyphs_file, const ProcessedLines& pro
 	{
 		all_glyphs_file << "\tGlyph( ";
 
-		all_glyphs_file << formatted(max_sizes[0], "{}, ", line.codepoint);
+		all_glyphs_file << formatter.format<0>(line, "{}, ");
 
 		all_glyphs_file << "{ ";
-		all_glyphs_file << formatted(max_sizes[1], "&AllGlyphs[{}], ", line.rotate_90_index);
-		all_glyphs_file << formatted(max_sizes[2], "&AllGlyphs[{}], ", line.rotate_180_index);
-		all_glyphs_file << formatted(max_sizes[3], "&AllGlyphs[{}], ", line.mirror_x_index);
-		all_glyphs_file << formatted(max_sizes[4], "&AllGlyphs[{}], ", line.mirror_y_index);
-		all_glyphs_file << formatted(max_sizes[5], "&AllGlyphs[{}], ", line.mirror_forward_diag_index);
-		all_glyphs_file << formatted(max_sizes[6], "&AllGlyphs[{}]", line.mirror_backward_diag_index);
+		all_glyphs_file << formatter.format<1>(line, "&AllGlyphs[{}], ");
+		all_glyphs_file << formatter.format<2>(line, "&AllGlyphs[{}], ");
+		all_glyphs_file << formatter.format<3>(line, "&AllGlyphs[{}], ");
+		all_glyphs_file << formatter.format<4>(line, "&AllGlyphs[{}], ");
+		all_glyphs_file << formatter.format<5>(line, "&AllGlyphs[{}], ");
+		all_glyphs_file << formatter.format<6>(line, "&AllGlyphs[{}]");
 		all_glyphs_file << " }, ";
 
-		all_glyphs_file << formatted(max_sizes[7], "Connection::{}, ", line.up_connection);
-		all_glyphs_file << formatted(max_sizes[8], "Connection::{}, ", line.down_connection);
-		all_glyphs_file << formatted(max_sizes[9], "Connection::{}, ", line.left_connection);
-		all_glyphs_file << formatted(max_sizes[10], "Connection::{}", line.right_connection);
+		all_glyphs_file << formatter.format<7>(line, "Connection::{}, ");
+		all_glyphs_file << formatter.format<8>(line, "Connection::{}, ");
+		all_glyphs_file << formatter.format<9>(line, "Connection::{}, ");
+		all_glyphs_file << formatter.format<10>(line, "Connection::{}");
 
 		all_glyphs_file << " ),\n";
 	}
@@ -73,16 +85,16 @@ void output_all_glyphs(std::ofstream& all_glyphs_file, const ProcessedLines& pro
 
 void output_unichar_to_glyphs(std::ofstream& unichar_to_glyphs_file, const CodepointToIndex& codepoint_to_index)
 {
-	const std::array<std::size_t, 2> max_sizes = get_max_sizes(codepoint_to_index);
+	const OutputFormatter formatter(codepoint_to_index);
 
 	unichar_to_glyphs_file << "inline const std::map<wxUniChar, const Glyph*> UnicharToGlyph =\n";
 	unichar_to_glyphs_file << "{\n";
 
-	for (auto& [codepoint, glyph] : codepoint_to_index)
+	for (auto& line : codepoint_to_index)
 	{
 		unichar_to_glyphs_file << "\t{ ";
-		unichar_to_glyphs_file << formatted(max_sizes[0], "wxUniChar({}), ", codepoint);
-		unichar_to_glyphs_file << formatted(max_sizes[1], "&AllGlyphs[{}]", glyph);
+		unichar_to_glyphs_file << formatter.format<0>(line, "wxUniChar({}), ");
+		unichar_to_glyphs_file << formatter.format<1>(line, "&AllGlyphs[{}]");
 		unichar_to_glyphs_file << " },\n";
 	}
 
