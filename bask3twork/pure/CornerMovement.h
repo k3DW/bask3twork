db6b586e02460338951f37735fc1d6047cff203d
Braden Ganetsky <braden.ganetsky@gmail.com>
2024-02-02 18:06:43 -0600

    #75: Add more Corner and Movement functionality, and Point and Selection functionality

--- bask3twork/pure/CornerMovement.h
+++ bask3twork/pure/CornerMovement.h
@@ -15,3 +15,20 @@ enum class Movement
 	left,
 	right,
 };
+
+struct CornerMovement
+{
+	Corner corner;
+	Movement movement;
+
+	/// This operator allows CornerMovement to be used in switch statements
+	constexpr operator int() const
+	{
+		return (static_cast<int>(corner) << 2) | (static_cast<int>(movement));
+	}	
+};
+
+constexpr CornerMovement operator|(Corner corner, Movement movement)
+{
+	return { corner, movement };
+}
